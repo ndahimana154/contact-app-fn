32d902060acc51719a94d45a22cc4ea36ee86d6f
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-10-25 21:48:00 +0200

    Set up project with Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+function App() {
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-200">
+      <h1 className="text-4xl font-bold text-center text-blue-700">Hello, Tailwind with Vite!</h1>
+    </div>
+  )
+}
+
+export default App
